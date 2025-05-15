fe9a8c89a2e41172ed85f6abbf25fe3e4c748858
Rodoshix <102111944+Rodoshix@users.noreply.github.com>
2025-05-14 23:47:14 -0400

    Update AuthController.java

--- src/main/java/com/ecomarket/usuariosauth/controller/AuthController.java
+++ src/main/java/com/ecomarket/usuariosauth/controller/AuthController.java
@@ -6,7 +6,11 @@ import com.ecomarket.usuariosauth.security.JwtUtil;
 import com.ecomarket.usuariosauth.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 import java.util.Optional;
 
